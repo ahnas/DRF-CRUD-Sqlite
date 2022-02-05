2fb846b4634794207ecb6330951800de3817702b
Ahnas <manuahnas@gmail.com>
2022-02-05 19:11:35 +0530

    put View

--- employeeapi/views.py
+++ employeeapi/views.py
@@ -1,3 +1,23 @@
+from serializers import EmployeeSerializer
 from django.shortcuts import render
+from rest_framework.response import Response
+
 
 # Create your views here.
+from .models import Employee
+
+
+# def put(self, request, *args, **kwargs):
+#     emp_object = Employee.objects.get()
+
+
+#     data = request.data
+
+#     emp_object.fullname = data["fullname"]
+#     emp_object.emp_code = data["emp_code"]
+#     emp_object.mobile = data["mobile"]
+    
+#     emp_object.save()
+
+#     serializer = EmployeeSerializer(emp_object)
+#     return Response(serializer.data)
