ccce80b43d6a5d93b971c52e43ad2b79ca900b47
Ahnas <manuahnas@gmail.com>
2022-02-05 19:24:40 +0530

    modelfields change

--- employeeapi/models.py
+++ employeeapi/models.py
@@ -1,15 +1,19 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
 
 
 class Employee(models.Model):
-    fullname = models.CharField(max_length=100)
-    emp_code = models.CharField(max_length=100)
-    mobile = models.CharField(max_length=100)
-
+    checked = models.BooleanField(default=True)
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    age = models.IntegerField()
+    description = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
-        return str(self.fullname)
+        return str(self.name)
 
 
 
