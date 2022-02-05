cb075c9801af792e6dda8edcc266d00a7738704c
Ahnas <manuahnas@gmail.com>
2022-02-05 18:17:56 +0530

    model create

--- employeeapi/models.py
+++ employeeapi/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Employee(models.Model):
+    fullname = models.CharField(max_length=100)
+    emp_code = models.CharField(max_length=100)
+    mobile = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.fullname)
